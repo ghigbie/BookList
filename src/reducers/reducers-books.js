de3048c445519acd26612a9a86e0779d0af5901c
George Higbie <gehigbie@gmail.com>
2018-03-14 15:32:44 +0000

    Add books reducer

---
+++ src/reducers/reducers-books.js
@@ -0,0 +1,7 @@
+export default () => (
+    [
+        {title: 'Fullstack React'},
+        {title: 'React Quickly'},
+        {title: 'How to Talk to Your Cat About Gun Safety'},
+        {title: 'You Need More Sleep'}
+    ]);
